b5ed36889a9564be1317736972c13cc5a9adfcd6
chang <2622310479@qq.com>
2023-10-30 16:37:31 +0800

    perf(mdn): Change skip MDN link mode

--- script/mdn.js
+++ script/mdn.js
@@ -1,25 +1,39 @@
 // ==UserScript==
 // @name         自动跳转 MDN 中文
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  点击 MDN 链接，自动跳转至中文路由
 // @author       Ares-Chang
 // @match        https://developer.mozilla.org/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mozilla.org
+// @link         https://github.com/Ares-Chang/tampermonkey
 // @grant        none
 // ==/UserScript==
-(function () {
+;(function () {
   'use strict'
-
-  const button = document.querySelector('#languages-switcher-button')
 
   const url = window.location.href
 
-  if (!url.includes('/en-US/', 12))
-    return
+  if (url.includes('/zh-CN/')) return
 
-  const list = url.split('/en-US/')
-  const str = `${list[0]}/zh-CN/${list[1] || ''}`
+  const language = document.querySelector('#languages-switcher-button')
 
-  window.location.replace(str)
+  language.click() // 打开语言下拉菜单
+
+  setTimeout(() => {
+    const list = document.querySelectorAll('.languages-switcher-menu li')
+
+    // 获取 list 中内容为 "中文" 的元素
+    ;[...list].some(dom => {
+      const judge = dom.innerText.includes('中文')
+      if (judge) {
+        dom.querySelector('a').click()
+        return true
+      }
+
+      return false
+    })
+
+    language.click() // 关闭语言下拉菜单
+  }, 0)
 })()
